3c184b3c46b601c41c7d2ba63b093af8151f3a5b
Jetinat0r <jetin8t0r@gmail.com>
2025-02-19 17:48:44 -0500

    Basic Server Connection
    - Can now connect to a server
    - Proper error handling when server rejects or could not be found
    - User feedback when connecting and when a connection attempt fails
    - Prevent user from messing with UI during connection attempt

--- Assets/Scripts/ClientManager.cs
+++ Assets/Scripts/ClientManager.cs
@@ -4,9 +4,36 @@ using UnityEngine;
 using Riptide;
 using Riptide.Utils;
 
+public enum ClientToServerId : ushort
+{
+    requestDisconnect = 0,
+    joinInfo = 1,
+    playerPosRot = 2,
+    shoveStart = 3,
+    shoveExecute = 4,
+}
+
+public enum ServerToClientId : ushort
+{
+    disconnectWithReason = 0,
+    playerSpawnInfo = 1,
+    playerPosRot = 2,
+    playerShoveStart = 3,
+    playerShoveExecute = 4,
+}
+
 public class ClientManager : MonoBehaviour
 {
-    Client client;
+    //Maximum length in chars a player's name can be
+    //  The server can return a name longer due to adding identifiers (e.g. Player1, Player2)
+    //  This length ensures that with the server's additions the name is not too long for the display field
+    public const int PLAYER_NAME_MAX_LENGTH = 10;
+
+    public static ClientManager instance = null;
+
+    public Client client;
+    [SerializeField]
+    public List<Color> playerColors = new List<Color>();
     [SerializeField]
     private string serverAddress = "127.0.0.1";
     [SerializeField]
@@ -20,10 +47,26 @@ public class ClientManager : MonoBehaviour
         RiptideLogger.Initialize(Debug.Log, Debug.Log, Debug.LogWarning, Debug.LogError, false);
     }
 
+    private void Awake()
+    {
+        if (instance != null)
+        {
+            Destroy(gameObject);
+            return;
+        }
+        else
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+
+        client = new Client();
+    }
+
     // Start is called before the first frame update
     void Start()
     {
-        client = new Client();
+        
     }
 
     // Update is called once per frame
@@ -36,4 +79,8 @@ public class ClientManager : MonoBehaviour
     {
         client.Update();
     }
+
+    #region ServerEvents
+    
+    #endregion
 }
--- Assets/Scripts/MainMenuManager.cs
+++ Assets/Scripts/MainMenuManager.cs
@@ -4,9 +4,13 @@ using UnityEngine;
 using TMPro;
 using DG.Tweening;
 using UnityEngine.UI;
+using Riptide;
+using System;
 
 public class MainMenuManager : MonoBehaviour
 {
+    [SerializeField]
+    private RectTransform mainMenuPanel;
     [SerializeField]
     private TMP_InputField playerNameInputField;
     [SerializeField]
@@ -24,27 +28,48 @@ public class MainMenuManager : MonoBehaviour
     private Image loadingWheel;
     [SerializeField]
     private float loadingWheelRotationTime = 2.5f;
+    [SerializeField]
+    private Button playButton;
+    [SerializeField]
+    private Button settingsButton;
 
     // Start is called before the first frame update
     void Start()
     {
         loadingWheel.transform.DORotate(new Vector3(0f, 0f, -360f), loadingWheelRotationTime, RotateMode.LocalAxisAdd).SetRelative(true).SetEase(Ease.Linear).SetLoops(-1);
+        ClientManager.instance.client.Connected += DidConnect;
+        ClientManager.instance.client.ConnectionFailed += OnConnectionFailed;
     }
 
     //Called to bring the main menu back into view
-    public void EnterMainMenu()
+    public void ReturnToMainMenu()
     {
-
+        mainMenuPanel.gameObject.SetActive(true);
     }
 
     //Called to remove the main menu from view
-    public void ExitMainMenu()
+    public void LeaveMainMenu()
     {
+        SetUiConnectingState(false);
+        mainMenuPanel.gameObject.SetActive(false);
+    }
 
+    //What each UI element should do when attempting to connect to a server
+    //  Mainly used for throbber feedback and disabling input elements during a connection attempt
+    public void SetUiConnectingState(bool _connecting)
+    {
+        loadingWheel.gameObject.SetActive(_connecting);
+        playerNameInputField.interactable = !_connecting;
+        serverAddressInputField.interactable = !_connecting;
+        serverPortInputField.interactable = !_connecting;
+        playButton.interactable = !_connecting;
+        settingsButton.interactable = !_connecting;
     }
 
     public void FlashError(string _errorText)
     {
+        SetUiConnectingState(false);
+
         errorTextFadeTween?.Kill();
         clientErrorText.text = _errorText;
         clientErrorText.color = errorColor;
@@ -59,4 +84,69 @@ public class MainMenuManager : MonoBehaviour
             FlashError(serverAddressInputField.text);
         }
     }
+
+    #region ButtonEvents
+    //Attempts to connect to the server
+    //  Also sends preferred name and color
+    public void TryConnectToServer()
+    {
+        //Validate preferred name and color input
+        string _preferredPlayerName = playerNameInputField.text.Trim();
+        if (_preferredPlayerName.Length <= 0 || _preferredPlayerName.Length >= ClientManager.PLAYER_NAME_MAX_LENGTH)
+        {
+            FlashError("Name must be 1-10 characters long!");
+            return;
+        }
+
+        Message _message = Message.Create();
+        //Add preferred name
+        _message.AddString(_preferredPlayerName);
+        //Add preferred color index
+        _message.AddInt(0);
+
+        if (ClientManager.instance.client.Connect(serverAddressInputField.text + ":" + serverPortInputField.text, message: _message))
+        {
+            SetUiConnectingState(true);
+        }
+        else
+        {
+            FlashError("Could not resolve server address, check address and port!");
+        }
+    }
+    #endregion
+
+    #region ServerEvents
+    private void OnConnectionFailed(object _sender, ConnectionFailedEventArgs _e)
+    {
+        switch (_e.Reason)
+        {
+            case (RejectReason.ServerFull):
+                FlashError("Connection rejected: Server is full!");
+                break;
+            case (RejectReason.AlreadyConnected):
+                FlashError("Connection rejected: Already connected to server!");
+                Debug.LogError("Connection rejected: Already connected to server!");
+                break;
+            case (RejectReason.NoConnection):
+                FlashError("Connection rejected: Could not connect to server!");
+                break;
+            case (RejectReason.Custom):
+                if(_e.Message == null)
+                {
+                    FlashError("Connection rejected: Unknown custom rejection from Server!");
+                }
+                else
+                {
+                    FlashError("Connection rejected: " + _e.Message.GetString());
+                }
+                break;
+        }
+    }
+
+    private void DidConnect(object _sender, EventArgs _e)
+    {
+        LeaveMainMenu();
+        //SendPlayerInfo();
+    }
+    #endregion
 }
